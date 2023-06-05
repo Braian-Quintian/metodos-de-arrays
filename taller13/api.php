e43732d4e5a869d781c82317ac09ffe0824f7bef
Braian-Quintian <bquintianromero@gmail.com>
2023-06-05 14:41:56 -0500

    fix: :bug: Se arreglo la redirrecion del link

--- taller13/api.php
+++ taller13/api.php
@@ -5,8 +5,6 @@ $planetas = ['Mercurio', 'Venus', 'Tierra', 'Marte', 'Jupiter', 'Saturno', 'Uran
 // Paso 2: Invertir el orden de los planetas
 $planetasInvertidos = array_reverse($planetas);
 
-// Paso 3: Imprimir los planetas invertidos
-foreach ($planetasInvertidos as $planeta) {
-echo $planeta . "<br>";
-}
-?>
+// Paso 3: Redirigir al archivo index.php con los planetas invertidos
+header("Location: index.php?planetas=" . urlencode(implode(',', $planetasInvertidos)));
+exit();
