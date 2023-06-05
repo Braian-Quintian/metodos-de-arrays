4cc0d231c6a01af49c16286a505d13af9bc17d1d
Braian-Quintian <bquintianromero@gmail.com>
2023-06-05 14:35:48 -0500

    feat: :sparkles: Se crea la logica

---
+++ taller12/api.php
@@ -0,0 +1,11 @@
+<?php
+// Paso 1: Crear el array de planetas
+$planetas = ['Mercurio', 'Venus', 'Tierra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno'];
+
+// Paso 2: Invertir el orden de los planetas
+$planetasInvertidos = array_reverse($planetas);
+
+// Paso 3: Redirigir al archivo index.php con los planetas invertidos
+header("Location: index.php?planetas=" . urlencode(implode(',', $planetasInvertidos)));
+exit();
+?>
