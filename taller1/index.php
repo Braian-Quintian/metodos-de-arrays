48f0c71cd8930d9774f2b98718506d59dd628b4f
Braian-Quintian <bquintianromero@gmail.com>
2023-06-04 23:33:00 -0500

    feat: :sparkles: Se cambia el titulo, y la data del navbar

--- taller1/index.php
+++ taller1/index.php
@@ -5,8 +5,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Talleres | Inicio</title>
+    <title>Talleres | 1</title>
+    <!-- CSS PRINCIPAL -->
     <link rel="stylesheet" href="../assets/css/style.css">
+    <!-- CSS SEGUNDARIO -->
+    <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 
 <body>
@@ -14,12 +17,12 @@
         <div class="header">
             <nav class="main-nav">
                 <ul>
-                    <li class="nav-item"><a href="../index.html" class="nav-link" data-link="incio">Inicio</a></li>
-                    <li class="nav-item"><a href="./index.php" class="nav-link" data-link="taller2">Taller 1</a></li>
-                    <li class="nav-item"><a href="./taller2/index.php" class="nav-link" data-link="taller3">Taller 2</a></li>
-                    <li class="nav-item"><a href="./taller3/index.php" class="nav-link" data-link="taller3">Taller 3</a></li>
-                    <li class="nav-item"><a href="./taller4/index.php" class="nav-link" data-link="taller4">Taller 4</a></li>
-                    <li class="nav-item"><a href="./taller5/index.php" class="nav-link" data-link="taller5">Taller 5</a></li>
+                    <li class="nav-item"><a href="../index.html" class="nav-link" data-link="inicio">Inicio</a></li>
+                    <li class="nav-item"><a href="index.php" class="nav-link" data-link="taller1">Taller 1</a></li>
+                    <li class="nav-item"><a href="../taller2/index.php" class="nav-link" data-link="taller2">Taller 2</a></li>
+                    <li class="nav-item"><a href="../taller3/index.php" class="nav-link" data-link="taller3">Taller 3</a></li>
+                    <li class="nav-item"><a href="../taller4/index.php" class="nav-link" data-link="taller4">Taller 4</a></li>
+                    <li class="nav-item"><a href="../taller5/index.php" class="nav-link" data-link="taller5">Taller 5</a></li>
                 </ul>
             </nav>
         </div>
