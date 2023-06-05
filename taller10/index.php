ea95b2a2680a24977793a7f16d730b191d07e928
Braian-Quintian <bquintianromero@gmail.com>
2023-06-05 14:35:27 -0500

    feat: :sparkles: Se actualizan los titulo

--- taller10/index.php
+++ taller10/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Talleres | 6</title>
+    <title>Talleres | 10</title>
     <!-- CSS PRINCIPAL -->
     <link rel="stylesheet" href="../assets/css/style.css">
     <!-- CSS SEGUNDARIO -->
